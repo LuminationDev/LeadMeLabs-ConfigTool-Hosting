52b14edf502af499a95270ad55ef411901bfff70
dkearns-lumination <dkearns@lumination.com.au>
2023-05-10 15:24:57 +0930

    Initialise config tool hosting server

---
+++ README.md
@@ -0,0 +1,3 @@
+# LeadMeLabs Config Tool Hosting Server
+
+A basic hosting server for downloading the latest version of Labs Config Tool
---
+++ server.js
@@ -0,0 +1,19 @@
+// Dependencies
+// =============================================================
+const compression = require('compression');
+const express = require('express');
+
+// Set up the Express App
+// =============================================================
+const app = express();
+const port = process.env.PORT || 8082;
+
+// Using compression npm to improve performance
+app.use(compression());
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/static/config-tool', express.static('applications/config-tool'))
+
+app.listen(port, () => console.log(`Listening on port ${port}!`));
